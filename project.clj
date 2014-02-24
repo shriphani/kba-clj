431ac4002e7892c6816272b29299fec485787cc7
Shriphani Palakodety <shriphanip@gmail.com>
2014-02-23 23:30:59 -0500

    added thrift dependencies and thrift generated java classes

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[ch.qos.logback/logback-classic "1.0.13"]
+                 [org.clojure/clojure "1.5.1"]
+                 [thrift-clj "0.2.1"]]
+  :java-source-paths ["src/gen-java"])
