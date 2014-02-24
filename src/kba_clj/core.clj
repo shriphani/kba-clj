da142228873489f08db389c4af888fcdab8aa11d
Shriphani Palakodety <shriphanip@gmail.com>
2014-02-24 00:14:21 -0500

    can now generate a stream

--- src/kba_clj/core.clj
+++ src/kba_clj/core.clj
@@ -1,6 +1,30 @@
-(ns kba-clj.core)
+(ns kba-clj.core
+  (:require [clojure.java.io :as io]
+            [thrift-clj.core :as thrift])
+  (:import [java.io FileInputStream]
+           [org.apache.thrift.protocol TBinaryProtocol]
+           [org.apache.thrift.transport
+            TIOStreamTransport
+            TTransport
+            TTransportException]
+           [streamcorpus StreamItem]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn file-stream
+  [a-file]
+  (let [itransport (-> a-file
+                       (FileInputStream.)
+                       (TIOStreamTransport.))
+        iprotocol  (new TBinaryProtocol itransport)]
+    (do
+      (.open itransport)
+      (take-while
+       identity
+       (repeatedly
+        (fn []
+          (let [item (new StreamItem)]
+            (try (do (.read item iprotocol)
+                     item)
+                 (catch TTransportException e (do (.close itransport)
+                                                  nil))))))))))
+
+
