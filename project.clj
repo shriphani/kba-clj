54bb89233149ea17b9eb9542e791dc2b216ff5f9
Shriphani Palakodety <shriphanip@gmail.com>
2014-02-24 13:14:56 -0500

    can run from command line now

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[byte-streams "0.1.9"]
                  [ch.qos.logback/logback-classic "1.0.13"]
+                 [org.bovinegenius/exploding-fish "0.3.4"]
                  [org.clojure/clojure "1.5.1"]
                  [thrift-clj "0.2.1"]]
-  :java-source-paths ["src/java"])
+  :java-source-paths ["src/java"]
+  :main kba-clj.core)
--- src/kba_clj/core.clj
+++ src/kba_clj/core.clj
@@ -1,5 +1,6 @@
 (ns kba-clj.core
   (:require [clojure.java.io :as io]
+            (org.bovinegenius [exploding-fish :as uri])
             [thrift-clj.core :as thrift])
   (:use [byte-streams])
   (:import [java.io FileInputStream]
@@ -38,3 +39,25 @@
        (.abs_url an-item)
        String))
     a-stream)))
+
+(defn stream-hosts
+  [a-link-stream]
+  (map uri/host a-link-stream))
+
+(defn -main
+  [& args]
+  (let [corpus-path (java.io.File. (first args))]
+    (doseq [host (flatten
+                  (map
+                   (fn [a-file]
+                     (-> a-file
+                         file-stream
+                         stream-links
+                         stream-hosts
+                         distinct))
+                   (filter
+                    #(re-find #"FORUM"
+                              (.getName %))
+                    (rest
+                     (file-seq corpus-path)))))]
+      (println host))))
